91b776e1dc49c1d84b23ab20c7c1b65a252204eb
Vlad Zuev <zuevvlad55@gmail.com>
2026-03-08 00:28:19 +0300

    Новая структура проекта

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-mod app;
-mod ui;
-mod serial;
-mod plot;
-
-use app::MyApp;
-
-fn main() -> eframe::Result<()> {
-
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([1000.0, 600.0]) // размер окна
-            .with_min_inner_size([900.0, 600.0]),
-        ..Default::default()
-    };
-
-    eframe::run_native(
-        "Fluxion",
-        options,
-        Box::new(|_cc| Box::new(MyApp::new())),
-    )
+fn main() {
+    println!("Hello, world!");
 }
